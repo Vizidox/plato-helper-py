10182c71b5fed40cb36c7556a4bbe75eb7eb4a66
Tiago Santos <tiago.santos@vizidox.com>
2020-02-08 10:35:56 +0000

    Add authentication for service

---
+++ templating_client_py/api.py
@@ -0,0 +1,58 @@
+import time
+from functools import wraps
+from http import HTTPStatus
+
+import requests
+
+
+class AuthenticationError(Exception): ...
+
+
+class TemplatingAPI:
+
+    def __init__(self, auth_server_url: str, templating_client_id: str, templating_client_secret: str):
+        self.templating_client_id = templating_client_id
+        self.templating_client_secret = templating_client_secret
+        self.auth_server_url = auth_server_url
+        self._token = None
+        self._token_expiration_date = None
+
+    @property
+    def token(self) -> str:
+
+        if self._token is None or time.time() > self._token_expiration_date:
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+
+            payload = {
+                "client_id": self.templating_client_id,
+                "client_secret": self.templating_client_secret,
+                "grant_type": "client_credentials"
+            }
+
+            response = requests.post(self.auth_server_url,
+                                     headers=headers,
+                                     data=payload)
+
+            if response.status_code != HTTPStatus.OK:
+                raise AuthenticationError(response.status_code)
+
+            json_response = response.json()
+            self._token = str(json_response['access_token'])
+            self._token_expiration_date = time.time() + int(json_response['expires_in'])
+
+        return self._token
+
+    @property
+    def header(self):
+        header = {
+            "Authorization": f"Bearer {self.token}",
+        }
+        return header
+
+    @property
+    def json_header(self):
+        header = self.header
+        header["Content-Type"] = "application/json"
+        return header
