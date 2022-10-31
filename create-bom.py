f46e2dd74ffaaef0e4536d65ed4f319636a27cbc
Joana Teixeira <joana.teixeira@vizidox.com>
2022-10-31 14:09:21 +0000

    Correct poetry installation in bom

--- create-bom.py
+++ create-bom.py
@@ -2,7 +2,7 @@
 
 import os
 
-os.system('curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -')
+os.system('curl -sSL https://install.python-poetry.org | python3 -')
 os.system('$HOME/.poetry/bin/poetry add cyclonedx-bom')
 os.system('$HOME/.poetry/bin/poetry export -f requirements.txt --output requirements.txt')
 os.system('rm bom.xml')
